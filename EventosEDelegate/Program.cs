8c4a350dd30c5371683e4eb5b05c864eb8872bd4
Roberto <rober@DESKTOP-JRDOGHA>
2021-11-22 16:04:19 -0300

    Eventos com delegate

--- EventosEDelegate/Program.cs
+++ EventosEDelegate/Program.cs
@@ -3,13 +3,63 @@
 namespace EventosEDelegate
 {
 
-    public class 
+    public class Treinamento
+    {
+        public int Vagas { get; set; }
+        private int vagasEmUso = 0;
+
+        public Treinamento(int vagas)
+        {
+            Vagas = vagas;
+            vagasEmUso = 0;
+        }
+
+        public void RegisrarVaga()
+        {
+            
+            if (vagasEmUso >= Vagas)
+            {
+                //Treinamento lotado
+                AoEncherTreinamento(EventArgs.Empty);
+            }
+            else
+            {
+                vagasEmUso++;
+                Console.WriteLine("Registrado no treinamento com sucesso!");
+            }
+        }
+
+        //Assinatura do evento... (delegate)
+        public event EventHandler TreinamentoLotadoEvent;
+
+        protected virtual void AoEncherTreinamento(EventArgs e)
+        {
+            EventHandler handler = TreinamentoLotadoEvent;
+            handler?.Invoke(this, e);
+        }
+
+    }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Treinamento trienamentoDotNet = new Treinamento(4);
+            trienamentoDotNet.TreinamentoLotadoEvent += AoEncherTreinamento;            
+            trienamentoDotNet.RegisrarVaga();
+            trienamentoDotNet.RegisrarVaga();
+            trienamentoDotNet.RegisrarVaga();
+            trienamentoDotNet.RegisrarVaga();
+            trienamentoDotNet.RegisrarVaga();
+            trienamentoDotNet.RegisrarVaga();
+            trienamentoDotNet.RegisrarVaga();
         }
+
+        static void AoEncherTreinamento(Object sender, EventArgs e)
+        {
+            Console.WriteLine("Sala Lotada");
+        }
+
+       
     }
 }
