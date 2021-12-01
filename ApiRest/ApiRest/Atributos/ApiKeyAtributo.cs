71d752eaf12eb5e13832e84e9141262dbd7a5893
Roberto <rober@DESKTOP-JRDOGHA>
2021-12-01 16:08:36 -0300

    Atorização com APIKey

---
+++ ApiRest/ApiRest/Atributos/ApiKeyAtributo.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ApiRest.Atributos
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ApiKeyAttribute : Attribute, IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(
+         ActionExecutingContext context,
+         ActionExecutionDelegate next)
+        {
+            if (!context.HttpContext.Request.Query.TryGetValue(Configuracao.ApiKeyName, out var extractedApiKey))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "ApiKey não encontrada"
+                };
+                return;
+            }
+
+            if (!Configuracao.ApiKey.Equals(extractedApiKey))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 403,
+                    Content = "Acesso não autorizado"
+                };
+                return;
+            }
+
+            await next();
+        }
+    }
+}
--- ApiRest/ApiRest/BasicaController.cs
+++ ApiRest/ApiRest/BasicaController.cs
@@ -1,4 +1,5 @@
-﻿using ApiRest.Entidades;
+﻿using ApiRest.Atributos;
+using ApiRest.Entidades;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -9,7 +10,10 @@ namespace ApiRest
     [ApiController]
     public class BasicaController : ControllerBase
     {
+
         // GET: api/Basica
+        
+        [ApiKey]
         [HttpGet]
         public IEnumerable<string> Get()
         {
