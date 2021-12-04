2d9a36aa139e3da2f05d09804cd5ea0036d031c5
Roberto <rober@DESKTOP-JRDOGHA>
2021-12-04 12:19:27 -0300

    Transação com entity framework

--- AcessoBancoDados/AcessoEntity/Program.cs
+++ AcessoBancoDados/AcessoEntity/Program.cs
@@ -8,6 +8,58 @@ namespace AcessoEntity
 {
     internal class Program
     {
+
+        static void TransacaoAlunoEndereco()
+        {
+            var contexto = new TESTEContext();
+            using var transaction = contexto.Database.BeginTransaction();
+
+            try
+            {
+                //INSERIR
+                Aluno aluno = new Aluno()
+                {
+                    Id = 0,
+                    Nome = "Adamastor ",
+                    Telefone = "3434-3434"
+                };
+
+                aluno.Enderecos = new List<Endereco>();
+                aluno.Enderecos.Add(new Endereco() { Logradouro = "Rua Oito", Cep = "32341-070", Numero = "213" });
+
+
+                contexto.Alunos.Add(aluno);
+                contexto.SaveChanges();
+
+
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                transaction.Rollback();
+            }
+        }
+
+        static void InserirAlunoEndereco()
+        {
+            var contexto = new TESTEContext();
+            //INSERIR
+            Aluno aluno = new Aluno()
+            {
+                Id = 0,
+                Nome = "Adamastor ",
+                Telefone = "3434-3434"
+            };
+
+            aluno.Enderecos = new List<Endereco>();
+            aluno.Enderecos.Add(new Endereco() { Logradouro = "Rua Oito", Cep = "32341-070", Numero = "213" });
+
+
+            contexto.Alunos.Add(aluno);
+            contexto.SaveChanges();
+        }
+
         static void Main(string[] args)
         {
             var context = new TESTEContext();
@@ -16,7 +68,7 @@ namespace AcessoEntity
 
 
             var listaAlunos = context.Alunos
-                        .Include(a => a.Telefones)
+                        .Include(a => a.Telefones.Where(t=>t.Id == 63))
                         .Include(a => a.Enderecos)
                         .ToList();
 
@@ -24,9 +76,9 @@ namespace AcessoEntity
 
             var aluno = new Aluno();
             aluno.Nome = "Napoleao Bonaparte";
-            aluno.Enderecos = new List<Endereco>() 
-            { 
-                new Endereco() { Logradouro = "Rua Oito", Cep="32341-070", Numero="345"  }                
+            aluno.Enderecos = new List<Endereco>()
+            {
+                new Endereco() { Logradouro = "Rua Nove", Cep="32341-070", Numero="345"  }
             };
             aluno.Telefones = new List<Telefone>()
             {
@@ -43,7 +95,7 @@ namespace AcessoEntity
             {
                 Console.WriteLine($"{item.Nome} {item.Id} {item.Telefone}");
 
-                foreach(var endereco in item.Enderecos)
+                foreach (var endereco in item.Enderecos)
                 {
                     Console.WriteLine($"ENDERECO: {endereco.Logradouro} {endereco.Cep} {endereco.Numero}");
                 }
